441b910141e01df2796bcfa591903d581951ed90
atocuden <81027782+atocuden@users.noreply.github.com>
2021-03-21 11:00:38 +0700

    Create tes.js

---
+++ js/tes.js
@@ -0,0 +1,24 @@
+  const counters = document.querySelectorAll('.counter');
+
+// this could work for multiple counters
+counters.forEach(counter => {
+  // start with 0 by default
+  counter.innerText = '0';
+  
+  const updateCounter = () => {
+    const target = +counter.getAttribute('data-target1');
+    const c = +counter.innerText;
+    
+    // get the 0.1% to speed up things
+    const increment = target / 100000;
+    
+    if(c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1)
+    } else {
+      counter.innerText = target;
+    }
+  };
+  
+  updateCounter();
+});
